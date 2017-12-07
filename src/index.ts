d34d1c1923753c71ae087f3dec7a86fc9ecb9265
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2017-12-07 09:31:56 +0100

    Better field API

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,83 @@
-import { type } from 'os'
-
 type PrimitiveTypeNames = 'null' | 'boolean' | 'number' | 'string' | 'date' | 'array' | 'object'
 
-export class Decoder<Value> {
+export interface IDecoder<Value> {
   readonly Type: Value
+
+  /** @throws TypeError in case decoding fails */
+  decode (json: string): Value
+}
+
+export class ObjectDecoder<Value> implements IDecoder<Value> {
+  readonly Type: Value
+
+  /** @throws TypeError in case value checking fails */
+  ensure: (value: any) => Value
+
   protected constructor () {}
-  /**
-   * @throws TypeError in case decoding fails
-   */
+
+  /** @throws TypeError in case value checking fails */
   decode (json: string): Value {
-    return this._decode(JSON.parse(json))
+    return this.ensure(JSON.parse(json))
   }
-  private _decode (value: any): Value {
-    throw new Error('Not Implemented')
+
+  field <Prev, FieldValue, Key extends string> (this: ObjectDecoder<Prev>, name: Key, fieldValue: Decoder<FieldValue>)
+  : ObjectDecoder<Prev & {[key in Key]: FieldValue}> {
+    return 0 as any
   }
 }
+
+export class Decoder<Value> implements IDecoder<Value> {
+  static object: ObjectDecoder<{}> = Object.assign(Object.create(ObjectDecoder.prototype), {
+    ensure: objectChecker
+  })
+
+  static null: Decoder<null> = Object.assign(Object.create(Decoder.prototype), {
+    ensure (value: any): null {
+      if (value !== null) {
+        throw new TypeError(`While decoding JSON, expected a null, got ${value}`)
+      }
+      return value
+    }
+  })
+
+  static boolean: Decoder<boolean> = Object.assign(Object.create(Decoder.prototype), {
+    ensure: primitiveChecker('boolean')
+  })
+
+  static string: Decoder<string> = Object.assign(Object.create(Decoder.prototype), {
+    ensure: primitiveChecker('string')
+  })
+
+  static number: Decoder<number> = Object.assign(Object.create(Decoder.prototype), {
+    ensure: primitiveChecker('number')
+  })
+
+  static date: Decoder<null> = Object.assign(Object.create(Decoder.prototype), {
+    ensure: dateChecker
+  })
+
+  /** @throws TypeError in case value checking fails */
+  ensure: (value: any) => Value
+
+  readonly Type: Value
+  protected constructor () {}
+
+  /** @throws TypeError in case value checking fails */
+  decode (json: string): Value {
+    return this.ensure(JSON.parse(json))
+  }
+}
+
+Decoder.object
+  .field('foo', Decoder.null)
+  .field('bar', Decoder.number)
+  .field('buzz', Decoder.string)
+  .decode('foo')
 
 const ISO_8601_REGEX = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i
 
-function nullChecker (value: any): null {
-  if (value !== null) {
-    throw new TypeError(`While decoding JSON, expected a null, got ${value}`)
-  }
-  return value
-}
-
-function primitiveChecker (type: 'boolean' | 'number' | 'string' | 'object') {
+// TODO make this more typesafe
+function primitiveChecker (type: 'boolean' | 'number' | 'string') {
   return function (value: any) {
     if (typeof value !== type) {
       throw new TypeError(`While decoding JSON, expected a ${type}, got ${value}`)
@@ -59,47 +111,4 @@ function arrayChecker<T> (elementDecoder: Decoder<T>): (value: any) => T[] {
     }
     return value
   }
-}
-
-export class FieldDecoder<Value> extends Decoder<Value> {}
-
-function decoderFactory (of: 'null'): Decoder<null>
-function decoderFactory (of: 'boolean'): Decoder<boolean>
-function decoderFactory (of: 'number'): Decoder<number>
-function decoderFactory (of: 'string'): Decoder<string>
-function decoderFactory (of: 'date'): Decoder<Date>
-function decoderFactory <T> (of: 'array', next: Decoder<T>): Decoder<T[]>
-function decoderFactory (of: 'object'): Decoder<{}>
-function decoderFactory <T extends object> (of: 'object', next: FieldDecoder<T>): Decoder<T>
-function decoderFactory <T, U> (of: PrimitiveTypeNames, next?: Decoder<U>): Decoder<T> {
-  const decoder: Decoder<T> = Object.create(Decoder.prototype)
-  if (of === 'null') {
-    (decoder as any)._decode = nullChecker
-  } else if (of === 'boolean' || of === 'number' || of === 'string') {
-    (decoder as any)._decode = primitiveChecker(of)
-  } else if (of === 'date') {
-    (decoder as any)._decode = dateChecker
-  } else if (of === 'array' && next) {
-    (decoder as any)._decode = arrayChecker(next)
-  } else if (of === 'object') {
-    if (!next) {
-      (decoder as any)._decode = objectChecker
-    }
-  }
-
-  return decoder
-}
-
-export const decoder = Object.assign(decoderFactory, {
-  nil: decoderFactory('null'),
-  null: decoderFactory('null'),
-  number: decoderFactory('number'),
-  string: decoderFactory('string'),
-  date: decoderFactory('date')
-})
-
-export function field<Value, Key extends string, Next extends object>
-  (name: Key, valueDecoder: Decoder<Value>, next?: FieldDecoder<Next>)
-  : FieldDecoder<{[key in Key]: Value} & Next> {
-  return valueDecoder as any
 }
