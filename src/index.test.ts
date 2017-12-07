712156aaacd6a7b5e0d97feebe7055700762df88
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2017-12-07 21:04:49 +0100

    Better API and object decoding

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,84 +1,123 @@
 import * as assert from 'assert'
-import { decoder, field } from './index'
+import { Decoder as D } from './index'
 
 describe('jdaughter', () => {
+  describe('Type', () => {
+    it('should throw when accessed', () => {
+      assert.throws(() => {
+        return D.null.Type
+      })
+    })
+  })
   describe('null', () => {
     it('should correctly parse', () => {
-      assert.strictEqual(decoder('null').decode('null'), null)
+      assert.strictEqual(
+        D.null.decode(JSON.stringify(null)),
+        null
+      )
     })
     it('should throw when it does not parse', () => {
       assert.throws(() => {
-        decoder('null').decode('"foo"')
+        D.null.decode(JSON.stringify('foo'))
       }, TypeError)
     })
   })
   describe('number', () => {
     it('should correctly parse', () => {
-      assert.strictEqual(decoder('number').decode('42'), 42)
+      assert.strictEqual(D.number.decode('42'), 42)
     })
     it('should throw when it does not parse', () => {
       assert.throws(() => {
-        decoder('number').decode('"foo"')
+        D.number.decode(JSON.stringify('foo'))
       }, TypeError)
     })
   })
   describe('string', () => {
     it('should correctly parse', () => {
-      assert.strictEqual(decoder('string').decode('"foo"'), 'foo')
+      assert.strictEqual(D.string.decode('"foo"'), 'foo')
     })
     it('should throw when it does not parse', () => {
       assert.throws(() => {
-        decoder('string').decode('false')
+        D.string.decode(JSON.stringify(false))
       }, TypeError)
     })
   })
   describe('Date', () => {
     it('should correctly parse timezone ISO 8601 dates', () => {
+      const date = new Date('2012-04-21T18:25:43-05:00')
       assert.deepEqual(
-        decoder('date').decode('"2012-04-21T18:25:43-05:00"'),
-        new Date('2012-04-21T18:25:43-05:00')
+        D.date.decode(JSON.stringify(date.toISOString())),
+        date
       )
     })
     it('should throw when it does not parse an arbitrary string', () => {
       assert.throws(() => {
-        decoder('date').decode('false')
+        D.date.decode(JSON.stringify(false))
       }, TypeError)
     })
   })
   describe('array', () => {
     it('should correctly parse', () => {
       assert.deepStrictEqual(
-        decoder('array', decoder('number')).decode('[1, 2, 3]'),
+        D.array(D.number).decode(JSON.stringify([1, 2, 3])),
         [1, 2, 3]
       )
     })
     it('should throw when it the value is not an array', () => {
       assert.throws(() => {
-        decoder('array', decoder('number')).decode('32')
+        D.array(D.number).decode(JSON.stringify(32))
       }, TypeError)
     })
     it('should throw when it is not an array of wrong elements', () => {
       assert.throws(() => {
-        decoder('array', decoder('number')).decode('["foo", "bar"]')
+        D.array(D.number).decode(JSON.stringify(['foo', 'bar']))
       }, TypeError)
     })
   })
   describe('object', () => {
     it('should support empty object parsing', () => {
       assert.deepStrictEqual(
-        decoder('object').decode('{}'),
+        D.object.decode(JSON.stringify({})),
         {}
       )
     })
     it('should throw when it is a primitive type', () => {
       assert.throws(() => {
-        decoder('object').decode('123')
+        D.object.decode(JSON.stringify(123))
       }, TypeError)
     })
     it('should throw when it is an array', () => {
       assert.throws(() => {
-        decoder('object').decode('[1, 2]')
+        D.object.decode(JSON.stringify([1, 2]))
       }, TypeError)
+    })
+    it('should support objects with fields', () => {
+      const expected = {
+        foo: 42,
+        bar: [true, false],
+        buzz: 'fsdf'
+      }
+      assert.deepStrictEqual(
+        D.object
+          .field('foo', D.number)
+          .field('bar', D.array(D.boolean))
+          .field('buzz', D.string)
+          .decode(JSON.stringify(expected)),
+        expected
+      )
+    })
+    it('should only return fields that are requested', () => {
+      const expected = {
+        foo: 42,
+        bar: [true, false],
+        buzz: 'asdf'
+      }
+      assert.deepStrictEqual(
+        D.object
+          .field('buzz', D.string)
+          .decode(JSON.stringify(expected)),
+        { buzz: 'asdf' }
+      )
     })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,114 +1,105 @@
-type PrimitiveTypeNames = 'null' | 'boolean' | 'number' | 'string' | 'date' | 'array' | 'object'
+function checkPrimitive <Value> (type: 'boolean' | 'number' | 'string')
+  : (value: any) => Value {
+  return (value: any) => {
+    if (typeof value !== type) {
+      throw new TypeError(`Expected value to be an object, got ${typeof value}`)
+    }
+    return value
+  }
+}
 
-export interface IDecoder<Value> {
-  readonly Type: Value
+const ISO_8601_REGEX = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i
+
+export abstract class AbstractDecoder<Value> {
+  get Type (): Value {
+    throw new TypeError('`Type` should never be used at runtime and only in `typeof` expression')
+  }
+
+  protected constructor () {
+    throw new TypeError('`Decoder` should never be constructed directly')
+  }
 
   /** @throws TypeError in case decoding fails */
-  decode (json: string): Value
-}
-
-export class ObjectDecoder<Value> implements IDecoder<Value> {
-  readonly Type: Value
-
-  /** @throws TypeError in case value checking fails */
-  ensure: (value: any) => Value
-
-  protected constructor () {}
-
-  /** @throws TypeError in case value checking fails */
   decode (json: string): Value {
-    return this.ensure(JSON.parse(json))
+    return this.doDecode(JSON.parse(json))
   }
 
-  field <Prev, FieldValue, Key extends string> (this: ObjectDecoder<Prev>, name: Key, fieldValue: Decoder<FieldValue>)
+  protected abstract doDecode (value: any): Value
+}
+
+export class ObjectDecoder<Value> extends AbstractDecoder<Value> {
+  field <Prev, FieldValue, Key extends string> (this: ObjectDecoder<Prev>, name: Key, fieldValue: AbstractDecoder<FieldValue>)
   : ObjectDecoder<Prev & {[key in Key]: FieldValue}> {
-    return 0 as any
+    const decoder: ObjectDecoder<Prev & {[key in Key]: FieldValue}> = Object.create(ObjectDecoder.prototype)
+
+    decoder.doDecode = (value) => {
+      const partialValue = this.doDecode(value) as any
+      partialValue[name] = (fieldValue as any).doDecode(value[name])
+      return partialValue
+    }
+
+    return decoder
+  }
+
+  protected doDecode (value: any): Value {
+    if (typeof value !== 'object') {
+      throw new TypeError(`Expected value to be an object, got ${typeof value}`)
+    }
+    if (Array.isArray(value)) {
+      throw new TypeError('Expected value to be an object, got an array')
+    }
+    return {} as any
   }
 }
 
-export class Decoder<Value> implements IDecoder<Value> {
-  static object: ObjectDecoder<{}> = Object.assign(Object.create(ObjectDecoder.prototype), {
-    ensure: objectChecker
+export class Decoder<Value> extends AbstractDecoder<Value> {
+  static boolean: Decoder<boolean> = Object.assign(Object.create(Decoder.prototype), {
+    doDecode: checkPrimitive('boolean')
+  })
+
+  static number: Decoder<number> = Object.assign(Object.create(Decoder.prototype), {
+    doDecode: checkPrimitive('number')
+  })
+
+  static string: Decoder<string> = Object.assign(Object.create(Decoder.prototype), {
+    doDecode: checkPrimitive('string')
   })
 
   static null: Decoder<null> = Object.assign(Object.create(Decoder.prototype), {
-    ensure (value: any): null {
+    doDecode (value: any): Date {
       if (value !== null) {
-        throw new TypeError(`While decoding JSON, expected a null, got ${value}`)
+        throw new TypeError(`Expected value to be null, got ${typeof value}`)
       }
       return value
     }
   })
 
-  static boolean: Decoder<boolean> = Object.assign(Object.create(Decoder.prototype), {
-    ensure: primitiveChecker('boolean')
-  })
-
-  static string: Decoder<string> = Object.assign(Object.create(Decoder.prototype), {
-    ensure: primitiveChecker('string')
-  })
-
-  static number: Decoder<number> = Object.assign(Object.create(Decoder.prototype), {
-    ensure: primitiveChecker('number')
-  })
-
-  static date: Decoder<null> = Object.assign(Object.create(Decoder.prototype), {
-    ensure: dateChecker
-  })
-
-  /** @throws TypeError in case value checking fails */
-  ensure: (value: any) => Value
-
-  readonly Type: Value
-  protected constructor () {}
-
-  /** @throws TypeError in case value checking fails */
-  decode (json: string): Value {
-    return this.ensure(JSON.parse(json))
-  }
-}
-
-Decoder.object
-  .field('foo', Decoder.null)
-  .field('bar', Decoder.number)
-  .field('buzz', Decoder.string)
-  .decode('foo')
-
-const ISO_8601_REGEX = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i
-
-// TODO make this more typesafe
-function primitiveChecker (type: 'boolean' | 'number' | 'string') {
-  return function (value: any) {
-    if (typeof value !== type) {
-      throw new TypeError(`While decoding JSON, expected a ${type}, got ${value}`)
+  static date: Decoder<Date> = Object.assign(Object.create(Decoder.prototype), {
+    doDecode (value: any): Date {
+      if (typeof value !== 'string' || !ISO_8601_REGEX.test(value)) {
+        throw new TypeError(`Expected value to be an ISO 8601 string, got ${typeof value}`)
+      }
+      return new Date(value)
     }
-    return value
-  } as any
-}
+  })
 
-function dateChecker (value: any): Date {
-  if (typeof value !== 'string' || !ISO_8601_REGEX.test(value)) {
-    throw new TypeError(`While decoding JSON, expected a ISO 8601 date, got ${value}`)
+  static object: ObjectDecoder<object> = Object.create(ObjectDecoder.prototype)
+
+  static array <Value> (of: Decoder<Value>): Decoder<Value[]> {
+    const decoder: Decoder<Value[]> = Object.create(Decoder.prototype)
+
+    decoder.doDecode = (value) => {
+      if (!Array.isArray(value)) {
+        throw new TypeError(`Expected value to be an array, got ${typeof value}`)
+      }
+
+      return value.map(of.doDecode)
+    }
+
+    return decoder
   }
 
-  return new Date(value)
-}
-
-function objectChecker (value: any): object {
-  if (typeof value !== 'object' || Array.isArray(value)) {
-    throw new TypeError(`While decoding JSON, expected an object, got ${value}`)
-  }
-  return value
-}
-
-function arrayChecker<T> (elementDecoder: Decoder<T>): (value: any) => T[] {
-  return function (value: any): T[] {
-    if (!Array.isArray(value)) {
-      throw new TypeError(`While decoding JSON, expected an array, got ${value}`)
-    }
-    for (let i = 0; i < value.length; ++i) {
-      (elementDecoder as any)._decode(value[i])
-    }
-    return value
+  protected doDecode (value: any): Value {
+    throw new TypeError('`doDecode` should never be called directly')
   }
 }
